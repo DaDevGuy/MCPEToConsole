5ffb158314150a2db7bdfc849d29edce94326746
Ehan <meoweverrr@gmail.com>
2022-05-15 16:50:10 +0500

    New Features
    Added Password Option.

--- src/MCPEtoConsole/DaDevGuy/Main.php
+++ src/MCPEtoConsole/DaDevGuy/Main.php
@@ -14,6 +14,14 @@ class Main extends PluginBase implements Listener
     public function onEnable(): void
     {
         $this->saveDefaultConfig();
+        if($this->getConfig()->get("config-ver") !== 1)
+        {
+            $this->getLogger()->info("WARNING! Config.yml of MCPEToConsole Is Not Updated Please Delete config.yml in plugin_data and restart the server!");
+        }
+        if($this->getConfig()->get("password") == "changeme")
+        {
+            $this->getLogger()->info("WARNING! You haven't changed default password of MCPEToConsole please change it immediately in config.yml");
+        }
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
@@ -30,15 +38,35 @@ class Main extends PluginBase implements Listener
             }
             if(isset($args[0]))
             {
-                if($this->getConfig()->get("console") == true){
-                    $this->getServer()->dispatchCommand(new ConsoleCommandSender($this->getServer(), $this->getServer()->getLanguage()), $args[0]);
+                if($args[0] !== $this->getConfig()->get("password"))
+                {
+                    $sender->sendMessage("Incorrect Password");
+                }
             }
-            else
+            if(!isset($args[1]))
             {
-                $this->getLogger()->info("console is not turned on in config.yml");
-                $sender->sendMessage("console is not turned on in config.yml");
+                $sender->sendMessage($this->getConfig()->get("usage"));
             }
-        }
+            if(isset($args[1]))
+            {
+                if($this->getConfig()->get("console") == true || "on")
+                {
+                    if($args[0] == $this->getConfig()->get("password"))
+                    {
+                        $this->getServer()->dispatchCommand(new ConsoleCommandSender($this->getServer(), $this->getServer()->getLanguage()), $args[1]);
+                    }
+                    else 
+                    {
+                        $sender->sendMessage("Incorrect Password");
+                    }
+                }
+                else 
+                {
+                    $this->getLogger()->info("console is not turned on in config.yml");
+                    $sender->sendMessage("console is not turned on in config.yml");
+                }
+              
+            }
         return true;
     }
     return false;
